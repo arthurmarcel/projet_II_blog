bffc70526d9421b7088105c51fb18528515c0b38
Arthur Marcel <arthur.marcel@etu.enseeiht.fr>
2012-04-23 17:37:31 +0200

    model and db building

--- db/migrate/20120417155827_create_posts.rb
+++ db/migrate/20120417155827_create_posts.rb
@@ -1,7 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-
+    	t.integer :user_id
+			t.string :title
+      t.text :body
       t.timestamps
     end
   end
